99b36a95d378db7ac12044376e72bfbfc2ff3eb6
Xenofon Kousidis <xenkou90@gmail.com>
2026-07-21 16:01:04 +0300

    chore: configure Prettier and ESLint integration

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import prettierConfig from 'eslint-config-prettier/flat';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -19,4 +20,5 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+  prettierConfig,
+]);
